615c336cdfc05f90e9d60afde73dbbf786584e08
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2024-03-23 16:11:21 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,9 @@
 const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'database',
+    user: 'username',
+    password: 'password',
+    
+});

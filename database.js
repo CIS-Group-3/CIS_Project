757420b686ce30a4cc9f2319820863f076cba8d7
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2024-04-03 16:10:27 -0400

    Connection to oracle

--- database.js
+++ database.js
@@ -1,9 +1,28 @@
-const mysql = require('mysql')
+const oracledb = require('oracledb');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'database',
-    user: 'username',
-    password: 'password',
-    
-});
+async function fun() {
+    let con;
+
+    try {
+
+        con = await oracledb.getConnection({
+            user: "abigail.lin",
+            password: "7yxtZs9hKMS0WxR0XV5MlrnE",
+            connectString: "oracle.cise.ufl.edu:1521/orcl"
+        });
+
+        //Example query to show connection to database
+        const data = await con.execute(
+            'SELECT * FROM DJIndex',
+        );
+        console.log(data.rows);
+
+    } catch (err) {
+        console.error(err);
+
+    }
+}
+
+fun();
+
+//Tutorial - https://www.youtube.com/watch?v=e_J8Q8YatB8
